421113c1e5912bdb39451935812a3b83d1a50b72
Vedant Pandey <kalashnikov@Vedants-Laptop.local>
2024-07-22 14:12:22 +0530

    first commit

---
+++ chatpdf-yt/src/app/page.tsx
@@ -0,0 +1,28 @@
+// import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const {userId}=await auth()
+  const isAuth= !!userId
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="flex flex-col items-center text-center">
+        <div className="flex items-center">
+          <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+          <UserButton afterSwitchSessionUrl="/"/>
+        </div>
+        <div className="flex mt-2">
+          {isAuth &&
+          <Button>Go to Chats</Button>}
+        </div>
+        <p>Join million of students</p>
+      </div>
+      </div>
+    </div>)
+    
+
+}
